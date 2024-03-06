b62d923726164301631d2d070903bbc5762c72de
Patrick McGreal <patrick.mcgreal.dev@gmail.com>
2024-03-06 10:31:01 +0100

    control-router init

--- demo/app.ts
+++ demo/app.ts
@@ -1,2 +1,35 @@
 import * as ControlRouter from "../src/control-router";
 
+const cr = ControlRouter.get();
+
+const metaControls = {
+
+  "Escape": () => {
+    console.log("menu");
+  },
+
+};
+
+const gameControls = {
+
+  "ArrowUp": () => {
+    console.log("up");
+  },
+  "ArrowDown": () => {
+    console.log("down");
+  },
+  "ArrowLeft": () => {
+    console.log("left");
+  },
+  "ArrowRight": () => {
+    console.log("right");
+  },
+
+};
+
+cr.addControlMap("game", {
+  ...metaControls,
+  ...gameControls,
+});
+
+cr.setControlMap("game");
