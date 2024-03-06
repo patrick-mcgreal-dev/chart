9600c5e8ca886a66132dc34d300aeef342416802
Patrick McGreal <patrick.mcgreal.dev@gmail.com>
2024-03-06 14:29:57 +0100

    crosshairs

--- www/cnv-worker.js
+++ www/cnv-worker.js
@@ -1,15 +1,37 @@
-let ctx, ctxW, ctxH;
+const chSize = 20;
+
+let ctx, ctxW, ctxH, centerX, centerY;
 let assets;
 
 function init(data) {
+
   ctx = data.offscreenCnv.getContext("2d");
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 3;
+
   ctxW = data.offscreenCnv.width;
   ctxH = data.offscreenCnv.height;
+  centerX = ctxW / 2;
+  centerY = ctxH / 2;
+
   assets = data.assets;
+
 }
 
 function draw(data) {
+
   ctx.drawImage(assets.map, data.x, data.y, ctxW, ctxH, 0, 0, ctxW, ctxH);
+
+  ctx.beginPath();
+  ctx.moveTo(centerX - chSize, centerY);
+  ctx.lineTo(centerX + chSize, centerY);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(centerX, centerY - chSize);
+  ctx.lineTo(centerX, centerY + chSize);
+  ctx.stroke();
+  
 }
 
 onmessage = (evt) => {
